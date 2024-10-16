15e6f2f00431b3f435fb2c6541bc02715c1973d3
Golu_singh <angelacarol12f@gmail.com>
2024-10-16 16:32:05 +0530

    update

--- src/components/common/CoverDesign.tsx
+++ src/components/common/CoverDesign.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Cover } from "@/components/ui/cover";
 
-export function CoverDesign({title,cover }) {
+
+interface CoverDesignProps {
+  title: string;
+  cover: string;
+}
+export function CoverDesign({title, cover }:CoverDesignProps) {
   return (
     <div>
       <h1 className="text-3xl  font-semibold  mx-auto  mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700">
